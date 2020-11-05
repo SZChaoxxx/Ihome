61cb75ace14f7f091437993b23c7ef95a3bc9333
Yao Fang <flavioy@live.com>
2020-11-05 18:03:48 +0800

    fix bug

--- ihome/ihome/apps/users/urls.py
+++ ihome/ihome/apps/users/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 urlpatterns = [
     path('users',views.RegisterView.as_view()),
     path('session',views.LoginView.as_view()),
-    path('user',views.UsernameCountView.as_view()),
     path('user/avatar',views.UpdateAvatar.as_view()),
     path('user/name',views.UpdateName.as_view()),
     path('user/auth',views.RealName.as_view()),
     path('user/houses', views.MyHousesView.as_view()),
+    path('user',views.UsernameCountView.as_view()),
 ]
