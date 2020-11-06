a7ad80cf85c0e6b01da6f34e8b9f4238a3d2c8a0
dengyumeng <weiwei@itcast.com>
2020-11-06 19:34:28 +0800

    修改订单列表

--- ihome/ihome/apps/order/views.py
+++ ihome/ihome/apps/order/views.py
@@ -5,6 +5,7 @@ import json, time
 from django.http import JsonResponse
 from django.shortcuts import render
 from homes.models import House
+import datetime
 from .models import Order
 from ihome.utils.views import LoginRequiredJsonMixin
 # Create your views here.
@@ -39,39 +40,48 @@ class AddorderView(LoginRequiredJsonMixin,View):
             # 登陆查询５号库
         # conn = get_redis_connection('order_information')
         # redis_orders = conn.hgetall('orders_%s' % user.id)
-        orders = Order.objects.filter(Q(house_id=house_id)&
-                                      Q(start_date=start_date))
+        order = Order.objects.filter(Q(house_id=house_id)&Q(begin_date=start_date))
 
 
         # 判断用户所选房屋是否被预定
-        if  orders:
+        if  order:
             return JsonResponse({'errno': '4003', 'errmsg': '数据已存在'})
 
         else:
 
             # 写入数据库
+            days = (datetime.datetime.strptime(end_date, '%Y-%m-%d') - datetime.datetime.strptime(start_date, '%Y-%m-%d')).days
+
+            house_price = int(house.price)/(int(house.acreage)/int(house.capacity))
             order = Order(
                 user_id=user.id,
                 house_id=house_id,
                 begin_date=start_date,
-                end_date=end_date
+                end_date=end_date,
+                house_price = house_price,
+                days = days,
+                amount = house_price*int(days),
+
             )
+
             order.save()
 
-        return JsonResponse({
-            "data": {
-                "order_id": orders.id
-            },
-            "errno": '0',
-            "errmsg": "下单成功"
-            })
+            return JsonResponse({
+                "data": {
+                    "order_id": order.id
+                },
+                "errno": '0',
+                "errmsg": "下单成功"
+                })
 
 
 
     def get(self, request):
         # 提取参数
         user = request.user
+
         role = request.GET.get('role')
+
         # 校验参数
         if not role:
             return JsonResponse({'errno': '4103', 'errmsg': '参数错误'})
@@ -79,39 +89,36 @@ class AddorderView(LoginRequiredJsonMixin,View):
 
         # 判断用户是否登陆
         # if user.is_authenticated:
-        #     # 读取数据库缓存
-
-        # # 便利添加字典
-
         # 构建响应列表
-        orders = []
+        if role == 'custom':
+            orders = []
 
         # ord_ids = Order.objects.all()
         # 便利并向列表中添加数据
         # for ord_id in ord_ids:
-        ord = Order.objects.all()
+            ord = Order.objects.filter(user_id=user.id)
 
 
-        for i in ord:
-            house = House.objects.get(pk=i.house_id)
-            orders.append({
-                'amount': i.amount,
-                'comment': i.comment,
-                'ctime': i.create_time,
-                'days': i.days,
-                "end_date": i.end_date,
+            for i in ord:
+                house = House.objects.get(id=i.house_id)
+                orders.append({
+                    'amount': i.amount,
+                    'comment': i.comment,
+                    'ctime': i.create_time,
+                    'days': i.days,
+                    "end_date": i.end_date,
 
-                "img_url":  "http://qj9kppiiy.hn-bkt.clouddn.com/%s" % house.index_image_url,
-                "order_id": i.id,
-                "start_date": i.begin_date,
-                "status": i.status,
-                "title": house.title
+                    "img_url":  "http://qj9kppiiy.hn-bkt.clouddn.com/%s" % house.index_image_url,
+                    "order_id": i.id,
+                    "start_date": i.begin_date,
+                    "status": i.status,
+                    "title": house.title
+                })
+            return JsonResponse({
+                "data":{ "orders": orders},
+                "errmsg": 'ok',
+                "errno": '0'
             })
-        return JsonResponse({
-            "data": orders,
-            "errmsg": 'ok',
-            "errno": '0'
-        })
 #todo 带验证
 class OrderStatus(LoginRequiredJsonMixin,View):
     def put(self, request, order_id):
